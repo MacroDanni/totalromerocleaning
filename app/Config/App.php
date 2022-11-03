060ea14985bae4135a1bbeeb1bc79c4a350b0c18
israel lopez <macro120788@gmail.com>
2022-11-03 10:54:51 -0600

    subir modificacion de url

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://todoromerocleaning.test/';
+    public $baseURL = 'https://todoromeroscleaning.herokuapp.com/';
 
     /**
      * --------------------------------------------------------------------------
